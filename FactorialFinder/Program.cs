2c39fa59a88ce7588081c7bf2ec0157d7c4ec4e7
Ben Hudson <16HudsoB@fulford.york.sch.uk>
2022-01-05 10:14:26 +0000

    added while loop to calculate n! of the users input and displays it

--- FactorialFinder/Program.cs
+++ FactorialFinder/Program.cs
@@ -11,14 +11,27 @@ namespace FactorialFinder
         static void Main(string[] args)
         {
             //The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. 
-            // https://stackoverflow.com/questions/29027000/c-sharp-to-calculate-factorial
+            
             //Solve this using both loops and recursion
 
+            int num, n;
+
             Console.WriteLine("Welcome to Factorial Finder");
-            Console.WriteLine("Please enter an integer");
-            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a number to get its factorial: ");
+            num = int.Parse(Console.ReadLine());
 
+            while (num > 0)
+            {
+                n = num;
+                for (int i = n - 1; i > 0; i--)
+                {
+                    n *= i;
+                }
+                Console.WriteLine("Factorial of {0}! = {1}\n", num, n);
+                num--;
+            }
 
+            Console.ReadLine();
         }
     }
 }
