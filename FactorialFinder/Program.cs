0a292f0fdf0c86ea0182a63aee45bf456c9e882c
Ben Hudson <16HudsoB@fulford.york.sch.uk>
2022-01-05 09:56:27 +0000

    added request for the user to enter an integer to find the factorial of

--- FactorialFinder/Program.cs
+++ FactorialFinder/Program.cs
@@ -10,7 +10,13 @@ namespace FactorialFinder
     {
         static void Main(string[] args)
         {
+            //The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. 
+
+            //Solve this using both loops and recursion
+
             Console.WriteLine("Welcome to Factorial Finder");
+            Console.WriteLine("Please enter an integer");
+            int num = int.Parse(Console.ReadLine());
         }
     }
 }
