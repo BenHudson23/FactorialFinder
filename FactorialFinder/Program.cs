d8ac0a31f792e6279dac06c04e5df523e8192e5c
Ben Hudson <16HudsoB@fulford.york.sch.uk>
2022-01-05 10:07:34 +0000

    added link to useful info for factorial calculation for code referal

--- FactorialFinder/Program.cs
+++ FactorialFinder/Program.cs
@@ -11,12 +11,14 @@ namespace FactorialFinder
         static void Main(string[] args)
         {
             //The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. 
-
+            // https://stackoverflow.com/questions/29027000/c-sharp-to-calculate-factorial
             //Solve this using both loops and recursion
 
             Console.WriteLine("Welcome to Factorial Finder");
             Console.WriteLine("Please enter an integer");
             int num = int.Parse(Console.ReadLine());
+
+
         }
     }
 }
